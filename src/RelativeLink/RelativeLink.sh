7b1b2a0c2678d167eb474aa12b85358d72509fd9
Jacob Appelbaum <jacob@appelbaum.net>
2009-08-26 22:33:28 +0000

    Add a very simple shell script that performs the same job as RelativeLink.c on Gnu/Linux systems.
    svn:r20381

---
+++ src/RelativeLink/RelativeLink.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Gnu/Linux does not really require something like RelativeLink.c
+# However, we do want to have the same look and feel with similiar features.
+# In the future, we may want this to be a C binary with a custom icon but at the moment
+# it's quite simple to just use a shell script
+#
+
+echo "Attemping to properly configure HOME..."
+export HOME=$PWD
+echo $HOME
+export LDPATH=$HOME/lib/
+echo "Attemping to properly configure LDPATH..."
+echo $LDPATH
+export LD_LIBRARY_PATH=$HOME/lib/
+echo "Attemping to properly configure LD_LIBRARY_PATH..."
+echo $LD_LIBRARY_PATH
+export DYLD_PRINT_LIBRARIES=1
+echo "Attemping to properly configure DYLD_PRINT_LIBRARIES..."
+echo $DYLD_PRINT_LIBRARIES
+# You may want to try this for debugging:
+# exec $HOME/bin/vidalia --loglevel debug --logfile vidalia-log \
+# --datadir .config/
+echo "Now attempting to launch the TBB for Linux..."
+exec $HOME/bin/vidalia --datadir .config/
+echo "Everything should be gone!"
