b6312579819c23b95d62c9121d31a46cb9a6961a
Jacob Appelbaum <jacob@appelbaum.net>
2009-08-28 02:24:58 +0000

    This actually properly installs Torbutton. How about them apples.
    svn:r20404

--- build-scripts/configure-firefox.sh
+++ build-scripts/configure-firefox.sh
@@ -6,17 +6,34 @@
 ## 4) Profit!
 ##
 
+# Here's where Make puts us
 STARTDIR="`pwd`";
+# This is the bundle we want to tamper with
 BUNDLE="$1";
 echo "BUNDLE is: $BUNDLE"
-EXTENSION=$2;
 
+# This is the full path to the extension we want to install
+EXTENSION="$STARTDIR/$2";
+
+# Here's a relative path to our custom Firefox
 FIREFOX="./App/firefox/firefox";
-FIREFOX_ARGS="-profile firefox -install-global-extension $EXTENSION";
+# Here's what we think should properly install a plugin
+#FIREFOX_ARGS="-profile default -install-global-extension ";
+FIREFOX_ARGS=" -install-global-extension ";
+
+# Now we'll switch into the bundle we wish to tamper with
 cd "$BUNDLE";
+# Now if we were running the bundle, we'd have our CWD as HOME
 export HOME=$PWD;
-echo "We're now in $PWD"
-echo "Attempting to run: $FIREFOX $FIREFOX_ARGS";
-$FIREFOX $FIREFOX_ARGS;
+echo "We're now in (HOME): $PWD"
+export LDPATH=$HOME/Lib/
+echo "Attemping to properly configure LDPATH..."
+echo $LDPATH
+export LD_LIBRARY_PATH=$HOME/Lib/
+echo "Attemping to properly configure LD_LIBRARY_PATH..."
+
+# Here's where we actually tell firefox to install this extension
+echo "Attempting to run: $FIREFOX $FIREFOX_ARGS $EXTENSION";
+$FIREFOX $FIREFOX_ARGS $EXTENSION;
 echo "Firefox said: $?";
 echo
