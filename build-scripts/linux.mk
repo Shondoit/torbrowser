f664edc19da163a3ccfa06ca437ee43acc72a2a4
Erinn Clark <erinn@torproject.org>
2011-04-30 01:46:17 +0300

    bump tbb linux to 1.1.8 for tor 0.2.2.25-alpha and firefox 3.6.17 releases

--- build-scripts/linux.mk
+++ build-scripts/linux.mk
@@ -42,7 +42,7 @@ TBB_FINAL=$(BUILT_DIR)/TBBL
 
 ## Versions for our source packages
 HTTPSEVERY_VER=0.9.9.development.4
-FIREFOX_VER=3.6.16
+FIREFOX_VER=3.6.17
 LIBEVENT_VER=2.0.10-stable
 LIBPNG_VER=1.4.3
 NOSCRIPT_VER=2.1.0.1
@@ -51,7 +51,7 @@ OTR_VER=3.2.0
 PIDGIN_VER=2.6.4
 POLIPO_VER=1.0.4.1
 QT_VER=4.6.2
-TOR_VER=0.2.2.24-alpha
+TOR_VER=0.2.2.25-alpha
 TORBUTTON_VER=1.2.5
 VIDALIA_VER=0.2.12
 ZLIB_VER=1.2.3
@@ -216,7 +216,7 @@ NAME=tor-browser
 DISTDIR=tbbl-dist
 
 ## Version and name of the compressed bundle (also used for source)
-VERSION=1.1.7-dev
+VERSION=1.1.8-dev
 DEFAULT_COMPRESSED_BASENAME=tor-browser-gnu-linux-$(ARCH_TYPE)-$(VERSION)-
 IM_COMPRESSED_BASENAME=tor-im-browser-gnu-linux-$(VERSION)-
 DEFAULT_COMPRESSED_NAME=$(DEFAULT_COMPRESSED_BASENAME)$(VERSION)
