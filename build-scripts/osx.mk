a2d694a6924140f73cef1549588c9b2b92e5dc63
Erinn Clark <erinn@torproject.org>
2011-06-09 04:00:18 -0300

    update tbb osx alpha to 2.2.28, bump tor to 0.2.2.28-beta, libevent to 2.0.12-stable, and extension versions as well

--- build-scripts/osx.mk
+++ build-scripts/osx.mk
@@ -144,7 +144,7 @@ NAME=TorBrowser
 DISTDIR=tbbosx-alpha-dist
 
 ## Version and name of the compressed bundle (also used for source)
-VERSION=2.2.27-1-alpha
+VERSION=$(RELEASE_VER)-1-alpha
 DEFAULT_COMPRESSED_BASENAME=TorBrowser-$(VERSION)-osx-$(ARCH_TYPE)-
 IM_COMPRESSED_BASENAME=TorBrowser-IM-$(VERSION)-
 DEFAULT_COMPRESSED_NAME=$(DEFAULT_COMPRESSED_BASENAME)
