7f5edf6876e20387977979a0c3b2a1fd6388c512
Erinn Clark <erinn@torproject.org>
2011-08-31 21:35:30 +0100

    bump libevent library names for 2.0.14-stable

--- build-scripts/linux.mk
+++ build-scripts/linux.mk
@@ -281,9 +281,9 @@ install-binaries:
 	# zlib
 	cp -d $(ZLIB)/libz.so $(ZLIB)/libz.so.1 $(ZLIB)/libz.so.1.2.5 $(LIBSDIR)/libz
 	# Libevent
-	cp -d $(LIBEVENT)/libevent-2.0.so.5 $(LIBEVENT)/libevent-2.0.so.5.1.2 $(LIBEVENT)/libevent_core.so \
-	   $(LIBEVENT)/libevent_core-2.0.so.5 $(LIBEVENT)/libevent_core-2.0.so.5.1.2 \
-	   $(LIBEVENT)/libevent_extra-2.0.so.5 $(LIBEVENT)/libevent_extra-2.0.so.5.1.2 \
+	cp -d $(LIBEVENT)/libevent-2.0.so.5 $(LIBEVENT)/libevent-2.0.so.5.1.3 $(LIBEVENT)/libevent_core.so \
+	   $(LIBEVENT)/libevent_core-2.0.so.5 $(LIBEVENT)/libevent_core-2.0.so.5.1.3 \
+	   $(LIBEVENT)/libevent_extra-2.0.so.5 $(LIBEVENT)/libevent_extra-2.0.so.5.1.3 \
 	   $(LIBEVENT)/libevent_extra.so $(LIBEVENT)/libevent.so $(LIBSDIR)
 	# libpng
 	cp -d $(LIBPNG)/libpng14.so* $(LIBSDIR) 
